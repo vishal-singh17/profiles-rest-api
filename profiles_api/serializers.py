32dc8ec55186164abb502831465b5b838f2997e8
Vishal Singh <imvishal.singh97@gmail.com>
2020-10-27 00:32:00 +0530

    Added user profile feed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -25,3 +25,11 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializers profile feed items"""
+
+    class Meta:
+        model= models.ProfileFeedItem
+        fields=('id','user_profile', 'status_text', 'created_on')
+        extra_kwargs={'user_profile':{'read_only':True}}
